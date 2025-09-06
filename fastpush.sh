e33400f415fd373a0e32f2bfdad4056bfbbcd73d
0xdya <0xdyaa@gmail.com>
2025-09-06 20:12:33 +0100

    new

---
+++ fastpush.sh
@@ -0,0 +1,40 @@
+
+
+# ========== fastpush ==========
+
+ascii_logo() {
+cat << "EOF"
+          ░█▀▀░█▀█░█▀▀░▀█▀░░░█▀█░█░█░█▀▀░█░█
+          ░█▀▀░█▀█░▀▀█░░█░░░░█▀▀░█░█░▀▀█░█▀█
+          ░▀░░░▀░▀░▀▀▀░░▀░░░░▀░░░▀▀▀░▀▀▀░▀░▀
+               by @0xdya
+EOF
+}
+
+check_dep() {
+  if ! command -v "$1" >/dev/null 2>&1; then
+    echo "install $1 first"
+    exit 1
+  fi
+}
+
+ascii_logo
+check_dep git
+check_dep node
+check_dep npm
+
+if [ ! -d .git ]; then
+  echo "fill not connected to repo "
+  git init
+
+  read -p "type username/repo" repo
+  git remote add origin "https://github.com/$repo/"
+
+  read -p "branch: " branch
+  git branch -M "$branch"
+fi
+
+git add .
+read -p "commit: " msg
+git commit -m "$msg"
+git push -u origin "$(git branch --show-current)"
